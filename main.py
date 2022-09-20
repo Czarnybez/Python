2ce1f4df47c7fddce78ae778ceb33fe390ac0bef
Czarnybez <madejski.w@gmail.com>
2022-09-20 11:48:39 +0200

    comit 6

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
     print('w tym miejscu zacznie sie nowa galaz')
     print('second brench')
     print('no to testujemy pulla!')
+    print('jeszcze jeden test')
 
 
     odp = [f"co ty jeszcze robisz w wupie!!, w {random.choice(propPraca)} jest wone miejsce",   #odp[0]
